26a4a0a2af452dd9026596b8ed3dfd87652703a7
dimis2112 <153327950+dimis2112@users.noreply.github.com>
2023-12-09 12:59:44 +0200

    Update script.js

--- client/js/script.js
+++ client/js/script.js
@@ -2650,8 +2650,8 @@ function handleSocket(socket) {
                 if (cell.id == myPlayer.cells[i].id) {
                     //  INTERPOLLATION 
                     myPlayer.cells[i].radius = (cell.radius + 0.1 * (myPlayer.cells[i].radius - cell.radius));
-                    //  myPlayer.cells[i].pos.x = (cell.pos.x + 0.5 * (myPlayer.cells[i].pos.x - cell.pos.x));
-                    // myPlayer.cells[i].pos.y = (cell.pos.y + 0.5 * (myPlayer.cells[i].pos.y - cell.pos.y));
+                    myPlayer.cells[i].pos.x = (cell.pos.x + 0.5 * (myPlayer.cells[i].pos.x - cell.pos.x));
+                    myPlayer.cells[i].pos.y = (cell.pos.y + 0.5 * (myPlayer.cells[i].pos.y - cell.pos.y));
                     //  myPlayer.cells[i].radius = (myPlayer.cells[i].radius + 0.0001 * (cell.radius - myPlayer.cells[i].radius));
                 }
             }
