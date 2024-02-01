62f203336a0aac9ea5d6e8498abc3004d34511ae
dimis2112 <153327950+dimis2112@users.noreply.github.com>
2024-02-01 16:28:50 +0200

    o

--- client/js/script.js
+++ client/js/script.js
@@ -2290,7 +2290,7 @@ let myViruses = [];
 let myMasses = {};
 let score_words = [];
 
-let interpollation_flag = false;
+let interpollation_flag = true;
 let prediction_flag = false;
 let score_words_flag = true;
 let broadcast_ups = 60;
